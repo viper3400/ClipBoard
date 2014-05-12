7292f63ecd09fd2a1d41ebc5d033ab54a77b139b
Yadong Wen <ralph.wen@gmail.com>
2014-05-12 21:43:42 +0800

    first working demo
    now can capture ctrl+c, copy from and paste to clipboard

---
+++ ClipBoard/MainForm.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace ClipBoard
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listView = new System.Windows.Forms.ListView();
+            this.indexColumnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.textColumnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // listView
+            // 
+            this.listView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.indexColumnHeader,
+            this.textColumnHeader});
+            this.listView.ForeColor = System.Drawing.Color.Green;
+            this.listView.FullRowSelect = true;
+            this.listView.GridLines = true;
+            this.listView.Location = new System.Drawing.Point(12, 12);
+            this.listView.MultiSelect = false;
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(260, 484);
+            this.listView.TabIndex = 0;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            this.listView.DoubleClick += new System.EventHandler(this.listView_DoubleClick);
+            // 
+            // indexColumnHeader
+            // 
+            this.indexColumnHeader.Text = "Index";
+            this.indexColumnHeader.Width = 40;
+            // 
+            // textColumnHeader
+            // 
+            this.textColumnHeader.Text = "Content";
+            this.textColumnHeader.Width = 600;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 508);
+            this.Controls.Add(this.listView);
+            this.Name = "MainForm";
+            this.Text = "ClipBoard";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.SizeChanged += new System.EventHandler(this.MainForm_SizeChanged);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.ColumnHeader indexColumnHeader;
+        private System.Windows.Forms.ColumnHeader textColumnHeader;
+        private System.Windows.Forms.Timer timer;
+
+
+    }
+}
+
---
+++ ClipBoard/MainForm.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClipBoard
+{
+    public partial class MainForm : Form
+    {
+        public ListView list;
+        public MainForm()
+        {
+            InitializeComponent();
+            list = this.listView;
+            list.Columns[1].Width = this.list.Width - 50;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+        public void updateList(Keys keys)
+        {
+            int index = this.list.Items.Count + 1;
+            //control-c pressed
+            if ((ModifierKeys & Keys.Control) == Keys.Control && keys == Keys.C)
+            {
+                string content = Clipboard.GetText();
+                if (content.Length != 0)
+                {
+                    if (list.Items.Count == 0) 
+                    {
+                        list.Items.Add(new ListViewItem(new string[] { index.ToString(), content }));                    
+                    }
+                    else
+                    {
+                        string prevCoutent = list.Items[list.Items.Count-1].SubItems[1].Text;
+                        if (!content.Equals(prevCoutent))
+                        {
+                            list.Items.Add(new ListViewItem(new string[] { index.ToString(), content })); 
+                        }
+                    }
+                }
+            }
+        }
+
+        private void MainForm_SizeChanged(object sender, EventArgs e)
+        {
+            list.Columns[1].Width = this.list.Width - 50;
+        }
+
+        private void listView_DoubleClick(object sender, EventArgs e)
+        {
+            copyTextToClipBoard();
+        }
+
+        private void copyTextToClipBoard()
+        {
+            int index = this.list.SelectedIndices[0];
+            string content = this.list.Items[index].SubItems[1].Text;
+            Clipboard.SetText(content);
+        }
+    }
+}
