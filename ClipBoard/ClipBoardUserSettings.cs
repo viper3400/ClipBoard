31e78e0c83e584b54f440ed325e4b3250def2f5b
viper3400 <viper_3400@gmx.de>
2017-05-15 22:08:28 +0200

    Added some description to user settings.

--- ClipBoard/ClipBoardUserSettings.cs
+++ ClipBoard/ClipBoardUserSettings.cs
@@ -22,6 +22,7 @@ namespace ClipBoard
         [UserScopedSetting]
         [Category("ClipBoard")]
         [DefaultSettingValue("10")]
+        [Description("Number of items which are shown in the frequent list. Set this to 0 to disable this feature.")]
         public int MaxItemsInFrequentList
         {
             get { return (int)this["MaxItemsInFrequentList"]; }
@@ -31,6 +32,7 @@ namespace ClipBoard
 
         [UserScopedSetting]
         [Category("ClipBoard")]
+        [Description("Path and filename of the file where your ClipBoard content will be saved.")]
         public string ContentFile
         {
             get
@@ -48,7 +50,7 @@ namespace ClipBoard
         [UserScopedSetting]
         [Category("ClipBoard")]
         [DefaultSettingValue("10000")]
-        [Description("Text with more chars than this value won't be handled by ClipBoard")]
+        [Description("Text with more chars than this value won't be handled by ClipBoard.")]
         public int MaxCopyTextLength
         {
             get { return (int)this["MaxCopyTextLength"]; }
