6249455ac9e700c2e0b30491575268ed35965c59
viper3400 <viper_3400@gmx.de>
2018-05-06 10:28:50 +0200

    Removed unused code.

--- ClipBoard/Program.cs
+++ ClipBoard/Program.cs
@@ -33,34 +33,12 @@ namespace ClipBoard
             var commandLineArgs = Environment.GetCommandLineArgs();
           
             SettingsFileName = commandLineArgs.Length > 1 ? commandLineArgs[1] : "";
-          //  _hookID = SetHook(_proc);
+            //  _hookID = SetHook(_proc);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             mf = new MainForm();
             Application.Run(mf);
             Win32Hooks.UnhookWindowsHookEx(_hookID);
         }
-
-        //private static IntPtr SetHook(Win32Hooks.LowLevelKeyboardProc proc)
-        //{
-        //    using (Process curProcess = Process.GetCurrentProcess())
-        //    using (ProcessModule curModule = curProcess.MainModule)
-        //    {
-        //        return Win32Hooks.SetWindowsHookEx(WH_KEYBOARD_LL, proc,
-        //            Win32Hooks.GetModuleHandle(curModule.ModuleName), 0);
-        //    }
-        //}
-
-        //private static IntPtr HookCallback(
-        //int nCode, IntPtr wParam, IntPtr lParam)
-        //{
-        //    if (nCode >= 0 && wParam == (IntPtr)Msgs.WM_KEYDOWN)
-        //    {
-        //        int vkCode = Marshal.ReadInt32(lParam);
-        //        Keys keys = (Keys)vkCode;
-        //        mf.keyPressedHandler(keys);
-        //    }
-        //    return Win32Hooks.CallNextHookEx(_hookID, nCode, wParam, lParam);
-        //}
     }
 }
